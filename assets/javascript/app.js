00fffa1c5e998ca307646246f7f084f2587cbae3
Ryan Lacon <ryan.lacon@gmail.com>
2019-07-11 09:34:33 -0400

    Clicking topic button generates a limit of 10 giphys and ratings are appended at beginning of each element

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,12 @@ var topics = [];
 
 // Attach event listener to topic submit button on page
 $("#submitTopic").on("click", function () {
+
     // Get value of topic input box using the id
-    var searchTerm = $('#topic-text').val();
+    var searchTerm = $('#topic-text').val().trim();
 
     // Step 1) Add to topics array (Javascript)
+    // (The push() method adds new items to the end of an array, and returns the new length)
     topics.push(searchTerm);
     console.log("topics: " + topics);
 
@@ -14,7 +16,7 @@ $("#submitTopic").on("click", function () {
     createTopicButtons();
 
     // Step 3) 
-    // Adding click event listeners to all elements with a class of "topic_button"
+    // Adding click event listeners to all elements with a class of "topic-buttons"
     // Attach event listeners to buttons that we just created
     $(document).on("click", ".topic_button", displayGiphy);
 });
@@ -22,47 +24,68 @@ $("#submitTopic").on("click", function () {
 function createTopicButtons() {
     // Create topic buttons and add to html page
     // Step 1) Iterate through the topics
-    $(".topic_buttons").empty();
+    $(".topic-buttons").empty();
     for (i = 0; i < topics.length; i++) {
+
         // Step 2) Create button element for each topic 
         var topicsDiv = $("<button>");
         topicsDiv.text(topics[i]);
+
         // Adds a class of movie to our button
         topicsDiv.addClass("topic_button");
+
         // Added a data-attribute
         topicsDiv.attr("data-name", topics[i]);
+
         // Step 3) Attach button element to page/div
-        $(".topic_buttons").append(topicsDiv);
+        $(".topic-buttons").append(topicsDiv);
     }
 }
 
 function displayGiphy() {
 
-    var giphy_topic = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=ZY8tJH2YGXWTQiTm3hhFf7T0cl9vLMyE&tag=" + giphy_topic;
+    var giphyTopic = $(this).attr("data-name");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=ZY8tJH2YGXWTQiTm3hhFf7T0cl9vLMyE&q=" + giphyTopic + "&limit=10";
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
-
+        console.log(giphyTopic)
         // Goal: Display giphy response on the page for the particular topic that was clicked on
 
         // Step 1) Create a div element that will contain all the giphy data 
-        var newDiv = $("<div>");
-        newDiv.attr("class", "myNewDiv");
+        // var topicDiv = $("<div>");
+        // $("<div>").addClass('topicDiv');
 
-        //   Step 2) Create a giphy element
-        var giphyElement = $("<img>");
-        giphyElement.attr("class", "giphyElement");
-        //   giphyElement.attr("src", response.data.embed_url);
-        giphyElement.attr("src", response.data.image_original_url);
 
-        //  Step 3) Attach giphy element to parent div created in step 1
-        newDiv.append(giphyElement);
+        //   Step 2) Create a giphy element and rating
+        var results = response.data;
 
-        // Step 4) Append to parent container
-        $("#giphy-container").append(newDiv);
+        for (var i = 0; i < results.length; i++) {
+            var topicDiv = $("<div>");
+            topicDiv.addClass('topicDiv');
+            var giphyElement = $("<img>");
+            var p = $("<p>");
+
+            p.text(results[i].rating);
+
+            giphyElement.addClass("giphyElement");
+            giphyElement.attr("src", results[i].images.fixed_height_small_still.url);
+          
+
+            //  Step 3) Attach giphy element to parent div created in step 1
+            topicDiv.append(giphyElement);
+            console.log(topicDiv);
+
+            topicDiv.append(p);
+
+            // Step 4) Attach to parent container
+            $("#giphy-container").append(topicDiv);
+
+                
+
+        }
     });
 }
